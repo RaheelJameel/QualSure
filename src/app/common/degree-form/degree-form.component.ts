64a7c538d21b5f67946369c01cfa4d82bca5d423
Raheel Jameel <raheel_jameel@yahoo.com>
2017-12-24 04:07:04 +0500

    feat(common|degree-form): completed degree-form component with input & output

--- src/app/common/degree-form/degree-form.component.ts
+++ src/app/common/degree-form/degree-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { FormGroup, FormArray, FormBuilder, Validators } from '@angular/forms';
 
 import { Degree } from './degree';
 
@@ -9,9 +10,38 @@ import { Degree } from './degree';
 })
 export class DegreeFormComponent implements OnInit {
 
-  constructor() { }
+  @Input() degreeModel: Degree;
+  @Output() degreeOutput: EventEmitter<Degree> = new EventEmitter<Degree>();
+
+  degreeForm: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.initializeForm();
+  }
+
+  private initializeForm() {
+    this.degreeForm = this.formBuilder.group({
+      firstName: [this.degreeModel ? this.degreeModel.firstName : '', Validators.required],
+      lastName: [this.degreeModel ? this.degreeModel.lastName : '', Validators.required],
+      degreeType: [this.degreeModel ? this.degreeModel.degreeType : '', Validators.required],
+      field: [this.degreeModel ? this.degreeModel.field : '', Validators.required],
+      gradYear: [this.degreeModel ? this.degreeModel.gradYear : '', Validators.required],
+      gpa: [this.degreeModel ? this.degreeModel.gpa : '', Validators.required],
+      fatherFirstName: [this.degreeModel ? this.degreeModel.fatherFirstName : '', Validators.required],
+      fatherLastName: [this.degreeModel ? this.degreeModel.fatherLastName : '', Validators.required]
+    });
+  }
+
+  submit() {
+    console.log('DegreeFormComponent: Submit called');
+    if (this.degreeForm.valid) {
+      console.log('DegreeFormComponent: degreeForm Valid; emitting output');
+      this.degreeOutput.emit(this.degreeForm.value);
+    }
   }
 
 }
